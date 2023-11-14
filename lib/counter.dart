0c7f959b07d4b08491f75646668a911e4c9bb380
Alexis Lopez <alg9105@gmail.com>
2023-11-13 22:12:11 -0600

    counter was added into a different file and integrated into main

---
+++ lib/counter.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+class CounterPage extends StatefulWidget {
+  const CounterPage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<CounterPage> createState() => _CounterPageState();
+}
+
+class _CounterPageState extends State<CounterPage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  void _decrementCounter() {
+    if (_counter > 0) {
+      setState(() {
+        _counter--;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: _incrementCounter,
+            tooltip: 'Increment',
+            child: const Icon(Icons.add),
+          ),
+          const SizedBox(height: 16), // Espacio entre los botones
+          FloatingActionButton(
+            onPressed: _decrementCounter,
+            tooltip: 'Decrement',
+            child: const Icon(Icons.remove),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:playground/counter.dart';
 
 void main() {
   runApp(const MyApp());
@@ -31,60 +32,124 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  final List<Widget> _mainContents = [
+    // Content for Home tab
+    Container(
+      color: Colors.yellow.shade100,
+      alignment: Alignment.center,
+      child: const CounterPage(title: 'Counter App')
+      // child: const Text(
+      //   'Home',
+      //   style: TextStyle(fontSize: 40),
+      // ),
+    ),
+    // Content for Feed tab
+    Container(
+      color: Colors.purple.shade100,
+      alignment: Alignment.center,
+      child: const Text(
+        'Feed',
+        style: TextStyle(fontSize: 40),
+      ),
+    ),
+    // Content for Favorites tab
+    Container(
+      color: Colors.red.shade100,
+      alignment: Alignment.center,
+      child: const Text(
+        'Favorites',
+        style: TextStyle(fontSize: 40),
+      ),
+    ),
+    // Content for Settings tab
+    Container(
+      color: Colors.pink.shade300,
+      alignment: Alignment.center,
+      child: const Text(
+        'Settings',
+        style: TextStyle(fontSize: 40),
+      ),
+    )
+  ];
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  void _decrementCounter() {
-    if (_counter > 0) {
-      setState(() {
-        _counter--;
-      });
-    }
-  }
+  int _selectedIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        crossAxisAlignment: CrossAxisAlignment.end,
+      appBar: AppBar(title: const Text('Test')),
+      bottomNavigationBar: MediaQuery.of(context).size.width < 640
+          ? BottomNavigationBar(
+          currentIndex: _selectedIndex,
+          unselectedItemColor: Colors.grey,
+          selectedItemColor: Colors.indigoAccent,
+          // called when one tab is selected
+          onTap: (int index) {
+            setState(() {
+              _selectedIndex = index;
+            });
+          },
+          // bottom tab items
+          items: const [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.home), label: 'Home'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.feed), label: 'Feed'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.favorite), label: 'Favorites'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.settings), label: 'Settings')
+          ])
+          : null,
+      body: Row(
+        mainAxisSize: MainAxisSize.max,
         children: [
-          FloatingActionButton(
-            onPressed: _incrementCounter,
-            tooltip: 'Increment',
-            child: const Icon(Icons.add),
-          ),
-          SizedBox(height: 16), // Espacio entre los botones
-          FloatingActionButton(
-            onPressed: _decrementCounter,
-            tooltip: 'Decrement',
-            child: const Icon(Icons.remove),
-          ),
+          if (MediaQuery.of(context).size.width >= 640)
+            NavigationRail(
+              minWidth: 55.0,
+              selectedIndex: _selectedIndex,
+              // Called when one tab is selected
+              onDestinationSelected: (int index) {
+                setState(() {
+                  _selectedIndex = index;
+                });
+              },
+              labelType: NavigationRailLabelType.all,
+              selectedLabelTextStyle: const TextStyle(
+                color: Colors.amber,
+              ),
+              leading: const Column(
+                children: [
+                  SizedBox(
+                    height: 8,
+                  ),
+                  CircleAvatar(
+                    radius: 20,
+                    child: Icon(Icons.person),
+                  ),
+                ],
+              ),
+              unselectedLabelTextStyle: const TextStyle(),
+              // navigation rail items
+              destinations: const [
+                NavigationRailDestination(
+                    icon: Icon(Icons.home), label: Text('Home')),
+                NavigationRailDestination(
+                    icon: Icon(Icons.feed), label: Text('Feed')),
+                NavigationRailDestination(
+                    icon: Icon(Icons.favorite), label: Text('Favorites')),
+                NavigationRailDestination(
+                    icon: Icon(Icons.settings), label: Text('Settings')),
+              ],
+            ),
+
+          // Main content
+          // This part is always shown
+          // You will see it on both small and wide screen
+          Expanded(child: _mainContents[_selectedIndex]),
         ],
       ),
     );
   }
+
 }
